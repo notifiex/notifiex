dc80bcd3685d28e38651d93c423c684f2170a48c
burntcarrot <aadhav.n1@gmail.com>
2022-05-09 22:56:31 +0530

    ci: add release workflow + update mix.exs
    Signed-off-by: burntcarrot <aadhav.n1@gmail.com>
    
    ci: update release workflow
    
    Signed-off-by: burntcarrot <aadhav.n1@gmail.com>
    
    chore: fix mix.exs files
    
    Signed-off-by: burntcarrot <aadhav.n1@gmail.com>
    
    fix: fix ex_doc dep in CI
    
    Signed-off-by: burntcarrot <aadhav.n1@gmail.com>

--- mix.exs
+++ mix.exs
@@ -1,29 +1,56 @@
 defmodule Notifiex.MixProject do
   use Mix.Project
 
-  def project do
+  def project() do
     [
       app: :notifiex,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.12",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      docs: docs(),
+      name: "Notifiex",
+      source_url: "https://github.com/burntcarrot/notifiex"
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.27", only: :dev, runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:httpoison, "~> 1.8"},
       {:poison, "~> 5.0"}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      source_url: "https://github.com/burntcarrot/notifiex",
+      extras: ["README.md"]
+    ]
+  end
+
+  defp description() do
+    "Notifiex helps in sending out notifications to multiple providers including Slack, etc."
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "notifiex",
+      # These are the default files included in the package
+      files: ~w(lib static .formatter.exs mix.exs README* LICENSE*),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/burntcarrot/notifiex"}
     ]
   end
 end
