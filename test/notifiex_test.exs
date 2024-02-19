cfc9116a485e05fc712d30a50c3ed470ca77fc49
andreascm <andreas.chrisna@gmail.com>
2024-02-19 15:53:08 +0700

    test: add test cases for missing keys

--- test/notifiex_test.exs
+++ test/notifiex_test.exs
@@ -19,4 +19,32 @@ defmodule NotifiexTest do
 
     assert result == {:error, {:mock_error, false}}
   end
+
+  test "Slack notification must not work with missing params" do
+    expected_result = {:error, {:missing_required_params}, nil}
+
+    assert expected_result == Notifiex.send(:slack, nil, nil)
+    assert expected_result == Notifiex.send(:slack, nil, %{})
+    assert expected_result == Notifiex.send(:slack, %{}, nil)
+    assert expected_result == Notifiex.send(:slack, %{}, %{})
+
+    assert expected_result == Notifiex.send(:slack, %{text: "sample", channel: "channel"}, %{})
+    assert expected_result == Notifiex.send(:slack, %{text: "sample"}, %{token: "token"})
+    assert expected_result == Notifiex.send(:slack, %{channel: "channel"}, %{token: "token"})
+    assert expected_result == Notifiex.send(:slack, %{}, %{token: "token"})
+    assert expected_result == Notifiex.send(:slack, %{text: "sample"}, %{})
+    assert expected_result == Notifiex.send(:slack, %{channel: "channel"}, %{})
+  end
+
+  test "Discord notification must not work with missing params" do
+    expected_result = {:error, {:missing_required_params}, nil}
+
+    assert expected_result == Notifiex.send(:discord, nil, nil)
+    assert expected_result == Notifiex.send(:discord, nil, %{})
+    assert expected_result == Notifiex.send(:discord, %{}, nil)
+    assert expected_result == Notifiex.send(:discord, %{}, %{})
+
+    assert expected_result == Notifiex.send(:discord, %{content: "sample"}, %{})
+    assert expected_result == Notifiex.send(:discord, %{}, %{webhook: "webhook"})
+  end
 end
